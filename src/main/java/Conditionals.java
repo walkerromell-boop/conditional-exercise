eb9ae5595e926001fdcab5218d1c2fbf066daa99
Romell Walker <walkerromell@gmail.com>
2025-09-29 11:22:57 -0400

    got up to the grades part 2

--- src/main/java/Conditionals.java
+++ src/main/java/Conditionals.java
@@ -16,36 +16,35 @@ public class Conditionals {
 //         TODO: Write an if statement that checks if age is >= 18
 //         If true, print "You are an adult."
             //ask user for there grade
-            System.out.println("Enter your grade (A-F): ");
-            String grade = scanner.next();
-            if (grade.equals("A")) {
-                System.out.println("Excellent");
-            } else if (grade.equals("B")) {
-                System.out.println("Good Job");
-            } else if (grade.equals("C")) {
-                System.out.println("You passed.");
-            } else if (grade.equals("D")) {
-                System.out.println("Barely passed");
-            } else if (grade.equals("F")) {
-                System.out.println("Failed");
-            } else {
-                System.out.println("Invalid Grade");
 
-            }
+        }
+
+        System.out.println("Enter your grade (A-F): ");
+        String grade = scanner.next();
+        if (grade.equals("A")) {
+            System.out.println("Excellent");
+        } else if (grade.equals("B")) {
+            System.out.println("Good Job");
+        } else if (grade.equals("C")) {
+            System.out.println("You passed.");
+        } else if (grade.equals("D")) {
+            System.out.println("Barely passed");
+        } else if (grade.equals("F")) {
+            System.out.println("Failed");
+        } else {
+            System.out.println("Invalid Grade");
+        }
 //            A → "Excellent!"
 //            B → "Good job!"
 //            C → "You passed."
 //            D → "Barely passed."
 //            F → "Failed."
-            // Ask the user for their grade
+        // Ask the user for their grade
 //            System.out.print("Enter your grade (A-F): ");
 //            String grade = scanner.next();
 //
 //// TODO: Use if-else if statements to print different messages
 //// based on the grade entered.
-        }
-
-
 
 
     }
