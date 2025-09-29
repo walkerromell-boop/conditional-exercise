cf80ef19d657ba9b68753fe029f4b6bcee2e8944
Romell Walker <walkerromell@gmail.com>
2025-09-29 13:39:26 -0400

    method practice

---
+++ src/main/java/com/pluralsight/MethodPractice.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class MethodPractice {
+
+
+    public static void main(String[] args) {
+        sayHi();
+        fish();
+        dance();
+    }
+
+    public static void sayHi() {
+        System.out.println("Hi");
+
+    }
+
+    public static void dance(){
+        System.out.println("WOOOOAHOOOOH");
+        System.out.println("WoAH");
+        System.out.println("Stop Hammer Time!");
+        System.out.println("Hammer Time!");
+    }
+
+    public static void fish(){
+        System.out.println("GEt out fishing rod");
+        System.out.println("Bait the hook");
+        System.out.println("Cast");
+        System.out.println("Wait patiently...");
+    }
+}
